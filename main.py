9fc713639a097602195d1dbf58d343dd9bb6c4b5
zhanghang <zhanghang@igengmei.com>
2019-01-04 19:34:26 +0800

    add apscheduler when at 9:40 then start scraping

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import redis
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-
 from news_spider.spiders.iyiou_com import ScanViewSpider
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-if __name__=='__main__':
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('cron', id='yi_ou_job', hour=9, minute=40)
+def spider_job():
     process = CrawlerProcess(get_project_settings())
     process.crawl('yiou_spider')
     process.start()
+
+
+sched.start()
--- news_spider/settings.py
+++ news_spider/settings.py
@@ -22,27 +22,27 @@ NEWSPIDER_MODULE = 'news_spider.spiders'
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 USER_AGENTS = [
     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
@@ -62,12 +62,18 @@ USER_AGENTS = [
 ]
 IPLIST = ["http://111.177.163.158:9999", "http://113.12.202.50:56886", "http://182.111.64.8:53364"]
 
-#使用scrapy_redis的调度器
+# 使用scrapy_redis的调度器
 SCHEDULER = "news_spider.scrapy_redis.scheduler.Scheduler"
 SCHEDULER_QUEUE_CLASS = 'news_spider.scrapy_redis.queue.SpiderPriorityQueue'
 SCHEDULER_PERSIST = True
 
-#使用scrapy_redis的去重方式使用scrapy_redis的调度器
+# 订阅管道
+SUBSCRIBE_CHANNEL = "news_spider.subscribe.channel"
+
+# 消息结果管道
+PUBLISH_CHANNEL = "news_spider.publish.channel"
+
+# 使用scrapy_redis的去重方式使用scrapy_redis的调度器
 REDIS_HOST = '127.0.0.1'
 REDIS_PORT = 6379
 
@@ -79,9 +85,9 @@ FILTER_DB = 0
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'news_spider.middlewares.NewsSpiderSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
@@ -94,15 +100,15 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'news_spider.MongoPipeline.MongoPipeline': 200,
-   # 'news_spider.pipelines.NewsSpiderPipeline': 300,
+    # 'news_spider.pipelines.NewsSpiderPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -114,17 +120,17 @@ AUTOTHROTTLE_START_DELAY = 5
 AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 MONGO_URI = 'mongodb://127.0.0.1:27017'
 MONGO_DATABASE = 'scrapy_news'
